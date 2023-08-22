90486fb39c705b63467a8e026c2501a37f87833f
Brand0n1 <16135647-Brand0n1@users.noreply.replit.com>
2023-08-22 15:39:44 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let http = require("http");
+
+let server = http.createServer(
+  function( req, res )
+  {
+    
+    let string = "You Are Not Logged In";
+    
+    if ( req.url === "/Dashboard" ) 
+    {
+      string = "Welcome To Your Dashboard";
+    } 
+
+    res.writeHead( 200, { "Content-Type": "text/plain" } );
+
+    res.end( string );
+  }
+);
+
+server.listen(8080, "0.0.0.0");
+
+console.log("Node Server Is Running");
